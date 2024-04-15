3f60677efcd608a0fd9caa62632ca1b80ee6d608
Dilshan <ndkumarawansha@gmail.com>
2024-04-15 17:41:59 +0530

    electron app added react

--- main.js
+++ main.js
@@ -1,19 +1,20 @@
-const {app,BrowserWindow} = require('electron');
+const { app, BrowserWindow } = require('electron');
 const url = require('url');
 const path = require('path');
 
 function createMainWindow() {
-    const mainWIndow = new BrowserWindow({
-        title: 'Electron',
-        width:1000,
-        height:600
-    });
-
-    const startUrl = url.format({
-        pathname:path.join(__dirname,'index.html'),
-        protocol:'file',
-    });
-    mainWIndow.loadURL(startUrl);
+  const mainWIndow = new BrowserWindow({
+    title: 'Electron',
+    width: 1000,
+    height: 600,
+    webPreferences: {webSecurity: false}
+  });
+  mainWIndow.webContents.openDevTools();
+  const startUrl = url.format({
+    pathname: path.join(__dirname, './app/build/index.html'),
+    protocol: 'file',
+  });
+  mainWIndow.loadURL(startUrl);
 }
 
 app.whenReady().then(createMainWindow);
